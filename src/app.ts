863350618fe464849e1e3c4f6be03dd6126048a4
so-amuk <amuk.saxena@oncehub.com>
2022-03-23 17:39:46 +0530

    hi text added

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('Hello Nodejs!');
+  res.send('Hello, Hi Nodejs!');
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
