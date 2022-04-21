cf7b6a0238f28609a961d5cacbdbc2ec3a3211d6
so-amuk <amuk.saxena@oncehub.com>
2022-04-21 12:05:42 +0530

    Message updated

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('hey Jenkins!');
+  res.send('hello World!');
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
